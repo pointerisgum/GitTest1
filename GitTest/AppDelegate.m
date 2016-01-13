53b57b2f7a86a6a96c1fe8172b837a9a7a3eb427
Young-Min Kim <pointerisgum@me.com>
2016-01-13 12:29:34 +0900

    ;;;붙임

--- GitTest/AppDelegate.m
+++ GitTest/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"Test");;;;;
+    NSLog(@"Test");;;;;;;;;;;;;;;;;;;;;
     return YES;
 }
 
